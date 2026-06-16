19bc24588116e4f40d1eea7f167d4ee246cc153a
Harsh Gadewar <your-personal-email@gmail.com>
2026-06-17 00:58:44 +0530

    implemented redis to store refresh token

--- Backend/controllers/auth/accessTokenController.js
+++ Backend/controllers/auth/accessTokenController.js
@@ -2,19 +2,23 @@ import { generateAccessTokenutil } from "../../utils/generateAccessToken.js";
 import jwt from "jsonwebtoken";
 
 export const generateAccessToken = async (req, res) => {
-  
-    const refreshToken = req.cookies.refreshToken;
+  const refreshToken = req.cookies.refreshToken;
 
-    if (!refreshToken) {
-      return res.status(400).json({ message: "Refresh token not found" });
-    }
+  if (!refreshToken) {
+    return res.status(400).json({ message: "Refresh token not found" });
+  }
 
-    const decoded = jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET);
+  const decoded = jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET);
 
-    const accessToken =generateAccessTokenutil(decoded.id);
+  const tokenExists = await redisClient.get(refreshToken);
 
-    return res.json({
-        accessToken
-    });
-  
+  if(!tokenExists){
+    return res.status(400).json({ message: "Invalid refresh token" });
+  }
+
+  const accessToken = generateAccessTokenutil(decoded.id);
+
+  return res.json({
+    accessToken,
+  });
 };
